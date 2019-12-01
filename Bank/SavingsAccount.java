e628a4a39cfea0d61b268d8d2fb476342c2be461
sujeetpalei8 <58160675+sujeetpalei8@users.noreply.github.com>
2019-12-01 22:58:29 +0530

    Add files via upload
    To calculate the interest of savings and current account.

---
+++ Bank/SavingsAccount.java
@@ -0,0 +1,23 @@
+class SavingsAccount extends Account{
+    private double minBalance=5000;
+    public void setMinBalance(double min){
+        minBalance=min;
+    }
+    public double getMinBalance(){
+        return minBalance;
+    }
+    public double calculateInterest(){
+        double i=0;
+        double in=0;
+        if(getBalance()>minBalance){
+            if(getBalance()>200000)
+            in=4;
+            else 
+            in=3.5;
+            i=getBalance()*getNoOfYears()*in/100;
+        }
+        
+        return i;
+    }
+    
+}
